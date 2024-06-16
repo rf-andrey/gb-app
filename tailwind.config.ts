28d74382af3da56d97cc306200c5d00756b4eba0
Andrey <andreyfontoura95@gmail.com>
2024-06-16 00:44:02 -0300

    chore: adiciona DaisyUI e remove layout inicial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -6,15 +7,10 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+  theme: {},
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["night"],
   },
-  plugins: [],
 };
 export default config;
